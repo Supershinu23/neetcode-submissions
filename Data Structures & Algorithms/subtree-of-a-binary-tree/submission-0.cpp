5119af63efd089a99c1844e945089dfcd885c82d
Shrinivas Digambar Sontakke <shrinivassontakke13@gmail.com>
2026-05-16 21:03:50 +0530

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool func(TreeNode* p, TreeNode* q){
+        if(!p && q!=nullptr)return false;
+        if(!q && p!=nullptr)return false;
+        if(!p && !q)return true;
+        if(p->val != q->val)return false;
+
+        bool f1 = func(p->left, q->left);
+        bool f2 = func(p->right, q->right);
+        return f1 && f2;
+    }
+
+    // bool helper(TreeNode* root, TreeNode* subRoot){
+    //     if(!root)return false;
+    //     if(root->val == subRoot->val && func(root, subroot))return true;
+    //     return helper(root->left, subRoot) || helper(root->right, subRoot);
+    // }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root)return false;
+        if(root->val == subRoot->val && func(root, subRoot))return true;
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);    }
+};
